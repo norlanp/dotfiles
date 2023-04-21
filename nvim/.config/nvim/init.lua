cee12157fd5a69bb6030b35611d94d5953a01cdf
Norlan Perdomo <norlan.perdomo@gmail.com>
2023-04-21 14:55:07 -0400

    closetag

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -173,7 +173,6 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
-      "windwp/nvim-ts-autotag",
     },
     config = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
@@ -297,11 +296,10 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
-  autotag = { enable = true },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
