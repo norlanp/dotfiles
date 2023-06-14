ed897fd6f81940d4f2eb7b9a7204a6609bd0be76
Norlan Perdomo <norlan.perdomo@gmail.com>
2023-06-14 17:00:05 -0400

    rg

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -261,6 +261,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
+  pickers = {
+    live_grep = {
+      additional_args = function()
+        return { "--hidden" }
+      end
+    },
+    grep_string = {
+      additional_args = function()
+        return { "--hidden" }
+      end
+    },
+  },
   defaults = {
     mappings = {
       i = {
@@ -285,12 +297,14 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', function()
+  require('telescope.builtin').find_files({ hidden = true })
+end, { desc = '[S]earch [F]iles' })
+
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sg',require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
--- vim.keymap.set('n', '<leader>sF', require('telescope.builtin').find_files, { desc = 'Search files, include hidden', hidden=true})
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
