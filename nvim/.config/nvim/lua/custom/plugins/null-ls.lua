cc704fc7730f536494e24695035c3ddae03d647c
Norlan Perdomo <norlan.perdomo@gmail.com>
2023-06-25 16:18:49 -0400

    null-ls

---
+++ nvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -0,0 +1,19 @@
+return {
+    "jose-elias-alvarez/null-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.gofumpt,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.prettier,
+
+                null_ls.builtins.diagnostics.eslint,
+                null_ls.builtins.diagnostics.flake8,
+                null_ls.builtins.diagnostics.mypy,
+            },
+        })
+    end,
+    requires = { "nvim-lua/plenary.nvim" },
+}
