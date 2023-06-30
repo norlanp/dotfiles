a137938e5e0c93689d3addc1cad6aafe1f8f9584
Norlan Perdomo <norlan.perdomo@gmail.com>
2023-06-29 21:18:47 -0400

    replace flake8 with ruff

--- nvim/.config/nvim/lua/custom/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -10,7 +10,7 @@ return {
                 null_ls.builtins.formatting.prettier,
 
                 null_ls.builtins.diagnostics.eslint,
-                null_ls.builtins.diagnostics.flake8,
+                null_ls.builtins.diagnostics.ruff,
                 null_ls.builtins.diagnostics.mypy,
             },
         })
