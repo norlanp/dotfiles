fc2c9e1e204d1a4c2efdf99ffe54f4a023d15312
Norlan Perdomo <norlan.perdomo@gmail.com>
2023-08-26 12:56:22 -0400

    dev

--- nvim/.config/nvim/after/plugin/remap.lua
+++ nvim/.config/nvim/after/plugin/remap.lua
@@ -9,10 +9,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
--- next greatest remap ever : asbjornHaland
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
@@ -20,7 +18,6 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
--- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
