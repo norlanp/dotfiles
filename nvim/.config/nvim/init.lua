86ac222c1b06e0fd9db00d998e0080c6ceba94fb
Norlan Perdomo <norlan.perdomo@gmail.com>
2024-06-09 00:17:09 -0400

    ruff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -444,21 +444,21 @@ local servers = {
   },
   pyright = {
     settings = {
-    pyright = {
-      -- Using Ruff's import organizer
-      disableOrganizeImports = true,
-    },
-    python = {
-      analysis = {
-        -- Ignore all files for analysis to exclusively use Ruff for linting
-        ignore = { '*' },
+      pyright = {
+        -- Using Ruff's import organizer
+        disableOrganizeImports = true,
+      },
+      python = {
+        analysis = {
+          -- Ignore all files for analysis to exclusively use Ruff for linting
+          ignore = { '*' },
+        },
       },
     },
   },
-  },
   rust_analyzer = {},
   tsserver = {},
-  ruff_lsp = {},
+  ruff = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
