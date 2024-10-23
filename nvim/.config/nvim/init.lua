99019fd6a4a8e64d10b12eb8d73ba42e36de3aa0
Norlan Perdomo <norlan.perdomo@gmail.com>
2024-10-23 18:39:15 -0400

    dev

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -128,6 +128,7 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       vim.g.nord_contrast = true
+      vim.g.nord_cursorline_transparent = false
       vim.cmd.colorscheme 'nord'
     end,
   },
