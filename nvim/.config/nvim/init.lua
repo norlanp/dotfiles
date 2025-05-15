c44de3a3931236de8ed9827106f828277803ff85
Norlan Perdomo <norlan.perdomo@gmail.com>
2025-05-15 19:17:26 -0400

    fix mason lsp config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -457,7 +457,6 @@ local servers = {
       },
     },
   },
-  rust_analyzer = {},
   ts_ls = {},
   ruff = {},
   lua_ls = {
@@ -482,14 +481,16 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
+mason_lspconfig.setup {
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = servers[server_name],
+      }
+    end,
+  }
 }
 
 -- nvim-cmp setup
