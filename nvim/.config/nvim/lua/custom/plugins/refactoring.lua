5f22214a8a3ee3c87d9c7e659d1384b4fd2323b6
Norlan Perdomo <norlan.perdomo@gmail.com>
2026-08-25 20:00:35 -0400

    fix: swap refactoring.nvim deps to async.nvim

--- nvim/.config/nvim/lua/custom/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/custom/plugins/refactoring.lua
@@ -1,8 +1,7 @@
 return {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter"
+        "lewis6991/async.nvim",
     },
     opts = {},
 }
